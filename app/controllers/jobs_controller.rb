a3f5eb9d75b7afe0ab323815ed467f1824d3cc5c
oswww <chaoyue03@gmail.com>
2017-04-09 22:17:42 +0100

    add hidden job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,7 +4,7 @@ class JobsController < ApplicationController
   before_action :check_admin, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false).recent
   end
 
   def show
@@ -51,6 +51,6 @@ class JobsController < ApplicationController
   end
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -4,4 +4,5 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0 }
   validates :wage_upper_bound, numericality: { greater_than_or_equal_to: :wage_lower_bound }
+  scope :recent, -> { order("created_at DESC")}
 end
