fd1c773e2631b1e7af18caa2f0e7c7506061d0db
oswww <chaoyue03@gmail.com>
2017-04-13 00:03:52 +0100

    remove unused path

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,5 @@
 class JobsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   before_action :find_job, only: [:show, :edit, :update, :destroy]
-  before_action :check_admin, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @jobs = case params[:order]
@@ -19,37 +17,6 @@ class JobsController < ApplicationController
       flash[:warning] = "This Job already archieved"
       redirect_to root_path
     end
-  end
-
-  def edit
-  end
-
-  def new
-    @job = Job.new
-  end
-
-  def create
-    @job = Job.new(job_params)
-
-    if @job.save
-      redirect_to jobs_path
-    else
-      render :new
-    end
-  end
-
-  def update
-    if @job.update(job_params)
-      redirect_to jobs_path, notice: "Update Success"
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @job.destroy
-
-    redirect_to jobs_path, alert: "Job Deleted"
   end
 
   private
